631c9a233c5f0168ce23271d0fe73df9bf09ed36
Jonesey86 <irj082024@gmail.com>
2025-02-14 10:33:10 +0100

    changes in program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
   public static void Main(string[] args)
   {
 
-    RecipeRepository repository = new RecipeRepository();
+    RecipeRepository repository = new RecipeRepository(); // this is the crud function (Create,Read,Update,Delete)
 
     while (true)
     {
